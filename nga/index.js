5e469c5c1f1d351e02e95156444c7d5f0f77048d
Skela <144936854+NotRimuru@users.noreply.github.com>
2025-02-07 12:13:59 +0100

    Add files via upload

--- nga/index.js
+++ nga/index.js
@@ -1,19 +1,43 @@
 const revolver = document.getElementById("revolver");
+const drum = document.getElementById("drum");
+const players = document.getElementsByClassName("person");
 
-const winningNum = Math.floor(Math.random() * 6);
-let bulletNum = 0;
-function shoot(){
-    if(bulletNum == winningNum){
-        revolver.textContent = "You won!";
-    }
-    else{
-        revolver.textContent = bulletNum;
-    }
+let winningNum = Math.floor(Math.random() * 6);
+let bulletNum = -1;
+function shoot(player){
+    if(bulletNum > winningNum){
+        if(player != 0)return;
+        bulletNum = -1;
+        winningNum = Math.floor(Math.random() * 6);
 
+        return;
+    }
     bulletNum ++;
+    console.log(bulletNum);
+
+    const outcome = player == 1 ? "lost" : "won";
+    if(bulletNum == winningNum){
+        console.log(`You ${outcome}!`);
+    }
 }
 
+function round(){
+    for(let i = 0; i < 2; i++){
+        shoot(i);
+    }
+}
+
+function spinDrum(){
+    winningNum = Math.floor(Math.random() * 6);
+    bulletNum = -1;
+    console.log(`Winning Number: ${winningNum}`);
+}
+
+drum.addEventListener("click", (e) => {
+    spinDrum();
+    e.stopPropagation();
+});
+
 revolver.addEventListener("click", () => {
-    shoot();
-    shoot();
+    round();
 });
